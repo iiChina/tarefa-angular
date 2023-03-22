f34b58a8360f4d81813d99987f49aa1fed3d923d
Leonardo Chinarelo <leonardo.chinarelo@hotmail.com>
2023-03-21 21:53:04 -0300

    Commit final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Componente01Component } from './componente01/componente01.component';
+import { Componente02Component } from './componente02/componente02.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: Componente01Component },
+  { path: 'second-component', component: Componente02Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
